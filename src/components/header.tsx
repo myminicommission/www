c31b2ae8a68d72021c86abb1d8b754337a2d658b
Steve Good <sgood@lanctr.com>
2021-02-15 17:51:32 -0800

    Added TailwindCSS and a start to the UI

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,41 +2,43 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+const Header = () => (
+  <header className="flex flex-shrink-0">
+    <div className="flex-shrink-0 px-4 py-3 bg-black w-64">
+      <Link to="/">
+        <span className="ml-4 text-xl font-medium text-white">
+          My Mini Commission
+        </span>
+      </Link>
+    </div>
+    <div className="flex-1 flex bg-black px-6 items-center justify-between">
+      <nav>
         <Link
           to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+          className="hover:bg-gray-600 inline-block text-sm font-medium text-white px-3 py-2 leading-none"
         >
-          {siteTitle}
+          Home
         </Link>
-      </h1>
+        <Link
+          to="/page-2/"
+          className="ml-2 hover:bg-gray-600 inline-block text-sm font-medium text-white px-3 py-2 leading-none"
+        >
+          Pricing
+        </Link>
+        <Link
+          to="/using-typescript/"
+          className="hover:bg-gray-600 inline-block text-sm font-medium text-white px-3 py-2 leading-none"
+        >
+          About
+        </Link>
+      </nav>
+      <div className="flex items-center">
+        <button className="ml-6 text-gray-400 hover:text-gray-200">
+          Login / Register
+        </button>
+      </div>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
---
+++ src/pages/commission/list.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const CommissionList = () => {
+  return <div>Commission List</div>
+}
+
+export default CommissionList
