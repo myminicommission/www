32eeb9295870cf0689af410e2e581cdfaf1fce51
Steve Good <sgood@lanctr.com>
2021-03-05 10:46:48 -0800

    Replaced lorem ipsum filler text on the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,16 @@ export default function Home() {
 
       <div className="lg:px-4">
         <div className="max-w-none lg:max-w-7xl mx-auto my-0 lg:my-16">
-          <Box header="My Mini Commission" subheader="Welcome!">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum illo
-            cupiditate molestias atque consequuntur ea quo cumque, odit velit
-            sint similique? Asperiores ratione aperiam tempora, alias corrupti
-            deleniti quaerat molestiae.
+          <Box header="My Mini Commission" subheader="Coming Soon!">
+            My Mini Commission is the brainchild of{" "}
+            <a href="https://twitch.tv/ExtremeModeration" target="_blank">
+              ExtremeModeration
+            </a>{" "}
+            and is being developed as a way to organize and communicate about
+            miniature commission painting. Originally designed to be a personal
+            tool, Extreme has decided to open his tool to the global miniature
+            painting community. Stay tuned for further developments (pun
+            intended)!
           </Box>
         </div>
       </div>
