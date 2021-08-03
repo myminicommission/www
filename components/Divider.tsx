3a52d6bd9bcf672b76f1c9fb38d1368e5a6b44be
Steve Good <sgood@lanctr.com>
2021-08-03 16:52:56 -0700

    Beginning to flesh out commission screen

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,6 +1,9 @@
-export default function Divider() {
+type DividerProps = {
+  className?: string;
+};
+export default function Divider({ className }: DividerProps) {
   return (
-    <div className="relative">
+    <div className={`relative ${className}`}>
       <div className="absolute inset-0 flex items-center" aria-hidden="true">
         <div className="w-full border-t border-gray-600" />
       </div>
